c75c928c6c2c09ed20f0dff06f6f5f6a25520fd9
Carson Powers <me@carsonp.net>
2024-07-10 14:47:54 -0500

    commutative DEFINE_ACC?

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -36,7 +36,7 @@ class UOp:
   src: Tuple[UOp, ...] = tuple()
   arg: Any = None
   def commutative(self) -> bool:
-    return self.op is UOps.ALU and self.arg in {BinaryOps.ADD, BinaryOps.MUL, BinaryOps.MAX, BinaryOps.CMPNE, BinaryOps.XOR}
+    return self.op is UOps.DEFINE_ACC or (UOps.ALU and self.arg in {BinaryOps.ADD, BinaryOps.MUL, BinaryOps.MAX, BinaryOps.CMPNE, BinaryOps.XOR})
   @functools.cached_property
   def cmp_tuple(self):
     # NOTE: this sort of DEFINE_VAR shouldn't have to be here. only for PTX
